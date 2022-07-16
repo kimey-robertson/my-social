a6d6070daacde1164881274d51245b13bcf6001c
Sol95 <solomonkr1995@gmail.com>
2022-07-16 15:07:47 +1000

    Added fixed Header and MainDisplay components

---
+++ client/my-social/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+        <div className='container' id="App-header">
+            <div className='row'>
+                <div className='col' id='header'>
+                    <p>My Social</p>
+                </div>
+            </div>
+        </div>
+  )
+}
---
+++ client/my-social/src/components/MainDisplay/MainDisplay.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MainDisplay.css';
+
+export default function MainDisplay() {
+  return (
+    <main>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    {/* <p>test</p> */}
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col'>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
